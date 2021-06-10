3567b4eb25fe55d42b0ffe495e51a50cdd0a2740
weijsh <weijsh@capitek.com.cn>
2021-06-10 10:42:30 +0800

    力扣3

--- src/main/java/无重复字符的最长子串.java
+++ src/main/java/无重复字符的最长子串.java
@@ -1,6 +1,56 @@
+import java.util.*;
+
 /*
 给定一个字符串,请你找出其中不含有重复字符的最长子串的长度
 */
 public class 无重复字符的最长子串 {
+    public static void main(String[] args) {
+        String s = "abcabcbb";
+        System.out.println(lengthOfLongestSubstring(s));
+    }
+
+
+
+
+    /*符的 ASCII 码值作为数组的下标, 数组存储该字符所在字符串的位置。适用于字符集比较小的情况,
+     因为我们会直接开辟和字符集等大的数组。*/
+    public static int lengthOfLongestSubstring(String s) {
+        int n = s.length(), ans = 0;
+        int[] index = new int[128];
+        for (int j = 0, i = 0; j < n; j++) {
+            i = Math.max(index[s.charAt(j)], i);
+            ans = Math.max(ans, j - i + 1);
+            index[s.charAt(j)] = j + 1; // (下标 + 1) 代表 i 要移动的下个位置
+        }
+        return ans;
+    }
+
+    public static int lengthOfLongestSubstring2(String s) {
+        int n = s.length(), ans = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int j = 0, i = 0; j < n; j++) {
+            if (map.containsKey(s.charAt(j))) {
+                i = Math.max(map.get(s.charAt(j)), i);
+            }
+            ans = Math.max(ans, j - i + 1);
+            map.put(s.charAt(j), j + 1); // 下标 + 1 代表 i 要移动的下个位置
+        }
+        return ans;
+    }
+
+    public static int lengthOfLongestSubstring3(String s) {
+        int n = s.length();
+        Set<Character> set = new HashSet<>();
+        int ans = 0, i = 0, j = 0;
+        while (i < n && j < n) {
+            if (!set.contains(s.charAt(j))) {
+                set.add(s.charAt(j++));
+                ans = Math.max(ans, j - i);
+            } else {
+                set.remove(s.charAt(i++));
+            }
+        }
+        return ans;
+    }
 
 }

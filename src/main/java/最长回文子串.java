3084fe8a73494fbef15f3115c7acfd0b1e8c979a
weijsh <weijsh@capitek.com.cn>
2021-06-17 14:58:02 +0800

    力扣5

--- src/main/java/最长回文子串.java
+++ src/main/java/最长回文子串.java
@@ -4,16 +4,12 @@
 
 public class 最长回文子串 {
     public static void main(String[] args) {
-        String s = "babbad";
+        String s = "bababad";
         System.out.println(longestPalindrome(s));
         System.out.println(longestPalindrome2(s));
         System.out.println(longestPalindrome3(s));
+        System.out.println(longestPalindrome4(s));
     }
-
-
-
-
-
 
 
     //暴力解法
@@ -84,9 +80,45 @@ public class 最长回文子串 {
         // 这里要小心，跳出 while 循环时，恰好满足 s.charAt(i) != s.charAt(j)，因此不能取 i，不能取 j
         return s.substring(i + 1, j);
     }
+    //动态规划
+    public static String longestPalindrome3(String s) {
+        int len = s.length();
+        if (len < 2) {
+            return s;
+        }
+        int maxLen = 1;
+        int start = 0;
+
+        //dp[i][j]表示s[i..j]是否为回文串
+        boolean [][] dp = new boolean[len][len];
+        for(int i =0;i<len;i++){
+            dp[i][i] = true;
+        }
+        char[] chars = s.toCharArray();
+        for(int j =1;j<len;j++){
+            for(int i = 0 ;i<j;i++){
+                if(chars[i]!=chars[j]){
+                    dp[i][j] = false;
+                }else{
+                    if(j-i<3){
+                        dp[i][j] = true;
+                    }else{
+                        dp[i][j] =  dp[i+1][j-1];
+                    }
+                }
+                //只要dp[i][j]==true成立,就表示子串s[i..j]是回文串，此时记录长度和起始位置
+                if(dp[i][j] && j-i+1>maxLen){
+                    maxLen = j-i+1;
+                    start = i;
+                }
+            }
+        }
+        return s.substring(start,start+maxLen);
+     }
+
 
     //Manacher 算法
-    public static String longestPalindrome3(String s) {
+    public static String longestPalindrome4(String s) {
         int len = s.length();
         if (len < 2) {
             return s;
